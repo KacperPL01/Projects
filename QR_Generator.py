85df53ce155827335fc8002cd1dad5cf16844303
Kacper Pobóg-Lenartowicz <100446049+KacperPL01@users.noreply.github.com>
2022-11-06 14:06:03 +0100

    Update QR_Generator.py

--- QR_Generator.py
+++ QR_Generator.py
@@ -4,4 +4,4 @@ data = input("Enter the text or link to generate QR code: ")
 
 qr = pyqrcode.create(data)
 
-qr.svg('qr_code.svg', scale = 10)
+qr.svg('E:\\Python Scripts\\QR_Code\\qr_code.svg', scale = 10)
